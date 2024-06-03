42c2fad41e2dca1d4e7793f24f947a6948c7bca8
scarlet <sarahscarlet641@gmail.com>
2024-06-03 10:37:45 +0300

    complete

--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -12,19 +12,19 @@ class TestStudent:
         '''says a brief greeting.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        student = Student()
+        student = Student("Alice")
         student.hello()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n")
+        assert captured_out.getvalue() == "Hey there I'm so excited to learn stuff.\n"
 
     def test_raises_hand(self):
         '''respectfully tries to get the teacher's attention.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        student = Student()
+        student = Student("Alice")
         student.raise_hand()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\n")
+        assert captured_out.getvalue() == "Pick me!\n"
 
 class TestChattyStudent:
     '''Class ChattyStudent in student.py'''
@@ -33,17 +33,19 @@ class TestChattyStudent:
         '''says a brief greeting, then tries to spoil a TV show.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        chatty_student = ChattyStudent()
+        chatty_student = ChattyStudent("Bob")
         chatty_student.hello()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n" +
-            "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n")
+        expected_output = "Hey there I'm so excited to learn stuff.\n" + \
+            "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+        assert captured_out.getvalue() == expected_output
 
     def test_raises_hand(self):
         '''respectfully tries to get the teacher's attention ten times.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        chatty_student = ChattyStudent()
+        chatty_student = ChattyStudent("Bob")
         chatty_student.raise_hand()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n")
+        expected_output = "Pick me!\n" * 10
+        assert captured_out.getvalue() == expected_output
